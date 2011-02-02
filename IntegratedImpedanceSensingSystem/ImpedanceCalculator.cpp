6326bc0203b010ad2f4069606a81535e985e9ffe
jaa_saaymi <jaa_saaymi@d83832b1-3638-4858-8a38-9f153b6b3474>
2011-02-02 06:29:57 +0000

    Changes during temperature/humidity vs. Gauge factor experiments.

--- IntegratedImpedanceSensingSystem/ImpedanceCalculator.cpp
+++ IntegratedImpedanceSensingSystem/ImpedanceCalculator.cpp
@@ -50,8 +50,9 @@ void ImpedanceCalculator::processChannel(int c, long L, ImpedanceMeasurement *m)
 		substractMean(x, L);
 		V = sumBasis(x, L);
 		absV = abs(V); angleV = arg(V);
-		angleV = angleV
-				- 2*M_PI*fc*(c - carrierChannel)/((double)(Nc)*fs);
+                angleV = angleV
+                         //- 2*M_PI*fc*(c - carrierChannel)/((double)(Nc)*fs);
+                         - 2*M_PI*fc*(c - carrierChannel)/((double)(Nc + 0.05)*fs);
 		//qDebug() << "Channel " << c << " [IMP]: " << absV << ", " << angleV;
 		V = std::polar(absV, angleV);
 		I = (Vc - V)/cI.SeriesR;
